f8728f89a3b08915674d78c3584279a859c6cdf2
adwxithc <adwaithjanardhanan0@gmail.com>
2024-06-13 00:35:54 +0530

    fix: fixed bug in video call connection breaking after  multiple continues call

--- src/redux/features/user/session/initialState.ts
+++ src/redux/features/user/session/initialState.ts
@@ -1,17 +1,19 @@
+
 export interface IInitialSessionState {
-    audioDevice:string;
-    videoDevice:string;
-    video:boolean;
-    audio:boolean;
- 
- }
- 
- export const sessionInitialState: IInitialSessionState = {
-   
-     audioDevice: 'default', //enumerate devices, chosen audio input device (we dont care about the output device)
-     videoDevice: 'default',
-     video:true,
-     audio:true
- 
- };
- 
+   audioDevice:string;
+   videoDevice:string;
+   video:boolean;
+   audio:boolean;
+   remoteUserId:string;
+  
+
+}
+
+export const sessionInitialState: IInitialSessionState = {
+  
+    audioDevice: 'default', //enumerate devices, chosen audio input device (we dont care about the output device)
+    videoDevice: 'default',
+    video:true,
+    audio:true,
+    remoteUserId:'',
+};
--- src/webRTC/peer.ts
+++ src/webRTC/peer.ts
@@ -1,99 +1,123 @@
-export interface IPeerService{
-    getOffer(): Promise<RTCSessionDescriptionInit | undefined>
-    getAnswer(offer: RTCSessionDescriptionInit): Promise<RTCSessionDescriptionInit | undefined>
-    setRemoteDescription(ans: RTCSessionDescriptionInit): Promise<void>
-    addTrack(track: MediaStreamTrack, stream: MediaStream): Promise<void>
-    getPeerConnection(): RTCPeerConnection | null
-    destroyPeerConnection(): void
+export interface IPeerService {
+    getOffer(): Promise<RTCSessionDescriptionInit | undefined>;
+    getAnswer(
+        offer: RTCSessionDescriptionInit
+    ): Promise<RTCSessionDescriptionInit | undefined>;
+    setRemoteDescription(ans: RTCSessionDescriptionInit): Promise<void>;
+    addTrack(track: MediaStreamTrack, stream: MediaStream): Promise<void>;
+    getPeerConnection(): RTCPeerConnection | null;
+    destroyPeerConnection(): void;
+    getLocalStream(): MediaStream | null;
 }
 
-class PeerService implements IPeerService{
-
-    private peer: RTCPeerConnection|null;
-    constructor(){
-        
+class PeerService implements IPeerService {
+    private peer: RTCPeerConnection | null;
+    constructor() {
         this.peer = new RTCPeerConnection({
             iceServers: [
-              {
-                urls: [
-                  "stun:stun.l.google.com:19302",
-                  "stun:global.stun.twilio.com:3478",
-                ],
-              },
+                {
+                    urls: [
+                        'stun:stun.l.google.com:19302',
+                        'stun:global.stun.twilio.com:3478',
+                    ],
+                },
             ],
-          });
-
-        
+        });
     }
 
-    async getOffer(){
-       try {
-        if(!this.peer) return
-        const offer = await this.peer.createOffer()
-        await this.peer.setLocalDescription( new RTCSessionDescription(offer))
+    async getOffer() {
+        if (!this.peer) return;
+        const offer = await this.peer.createOffer();
+        await this.peer.setLocalDescription(new RTCSessionDescription(offer));
         return offer;
-       } catch (error) {
-        console.log('error generation offer');
-        
-       }
     }
 
-    async getAnswer(offer:RTCSessionDescriptionInit){
-      try {
-        if(!this.peer) return 
-        await this.peer.setRemoteDescription(offer)
-        const ans = await this.peer.createAnswer()
-        await this.peer.setLocalDescription( new RTCSessionDescription(ans))
-        return ans
-      } catch (error) {
-        console.log('error creating answer',error);
-        
-      }
-        
+    getLocalStream() {
+        if (!this.peer) return null;
 
+        const localTracks = this.peer
+            .getSenders()
+            .map((sender) => sender.track);
+
+        // Filter out null tracks and tracks of kind 'audio' or 'video'
+        const filteredTracks = localTracks.filter(
+            (track) =>
+                track && (track.kind === 'video' || track.kind === 'audio')
+        );
+
+        if (filteredTracks.length > 0) {
+            // Create a new MediaStream with the filtered tracks
+            const stream = new MediaStream(
+                filteredTracks.filter((track) => track) as MediaStreamTrack[]
+            );
+            return stream;
+        } else {
+            return null;
+        }
     }
 
-    async setRemoteDescription(ans:RTCSessionDescriptionInit){
-        if(!this.peer || !ans) return 
+    async getAnswer(offer: RTCSessionDescriptionInit) {
+        if (!this.peer) return;
+        await this.peer.setRemoteDescription(offer);
+
+        const ans = await this.peer.createAnswer();
+        await this.peer.setLocalDescription(new RTCSessionDescription(ans));
+        return ans;
+    }
+
+    async setRemoteDescription(ans: RTCSessionDescriptionInit) {
+        if (!this.peer) return;
         try {
-            
-            
-            
-            await this.peer.setRemoteDescription(new RTCSessionDescription(ans));
+            await this.peer.setRemoteDescription(
+                new RTCSessionDescription(ans)
+            );
         } catch (error) {
             console.error('Error setting remote description:', error);
         }
     }
 
-    async addTrack(track:MediaStreamTrack,stream:MediaStream){
+    async addTrack(track: MediaStreamTrack, stream: MediaStream) {
         try {
-            this.peer?.addTrack(track,stream)
+            this.peer?.addTrack(track, stream);
         } catch (error) {
             console.log('error in add tracks', error);
-            
         }
-        
     }
     getPeerConnection() {
-        return this.peer ;
-      }
+        return this.peer;
+    }
 
     destroyPeerConnection() {
         if (this.peer) {
-          this.peer.ontrack = null;
-       
-          this.peer.onicecandidate = null;
-          this.peer.oniceconnectionstatechange = null;
-          this.peer.onsignalingstatechange = null;
-          this.peer.onicegatheringstatechange = null;
-          this.peer.onnegotiationneeded = null;
-          this.peer.getSenders().forEach((sender) => sender.track?.stop());
-          // this.peer.close();
+            this.peer.ontrack = null;
+
+            this.peer.onicecandidate = null;
+            this.peer.oniceconnectionstatechange = null;
+            this.peer.onsignalingstatechange = null;
+            this.peer.onicegatheringstatechange = null;
+            this.peer.onnegotiationneeded = null;
+            this.peer.getSenders().forEach((sender) => sender.track?.stop());
+
+            // this.peer.close();
         }
-     
-      }
+    }
 }
 
-const peerService = new PeerService();
+// const peerService = new PeerService();
 
-export default peerService;
+// export default PeerService;
+
+let peer: IPeerService | null = null;
+
+const getPeerConnection = () => {
+    console.log('gotted peer', peer);
+
+    if (peer !== null) return peer;
+    peer = new PeerService();
+    return peer;
+};
+export const resetPeerConnection = () => {
+    peer = null;
+};
+
+export default getPeerConnection;
